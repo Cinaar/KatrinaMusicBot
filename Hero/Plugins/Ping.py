8a7147521df31e5461dca08f28f4c5315b313e4c
Cinaar <106670406+Cinaar@users.noreply.github.com>
2022-08-08 08:34:25 +0400

    update Ping.py

--- Hero/Plugins/Ping.py
+++ Hero/Plugins/Ping.py
@@ -33,7 +33,7 @@ async def bot_sys_stats():
 async def ping(_, message):
     start = datetime.now()
     response = await message.reply_photo(
-        photo="https://telegra.ph/file/7e098161ccb85f4eab925.jpg",
+        photo="Utils/Gizli.JPEG",
         caption="🌸 ᴘɪɴɢ...",
     )
     uptime = await bot_sys_stats()
